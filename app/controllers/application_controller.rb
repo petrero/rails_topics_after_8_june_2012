6b71ccec1933945d159e74d4d520b8564b3ffda2
petrero <petrero@gmail.com>
2012-09-18 22:22:37 +0300

    enabling ssl connection on certain parts of the site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    #make sure that current user is checked over a secure connection or
+    #if connection is not secure is checked against the secure cookie
+    if !request.ssl? || cookies.signed[:secure_user_id] == "secure#{session[:user_id]}"
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
   end
   helper_method :current_user
 end
