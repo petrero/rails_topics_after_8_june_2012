1b4b05116af7eff8377cddf2410f8bb9b62fa1aa
petrero <petrero@gmail.com>
2012-09-22 00:24:29 +0300

    Resolving insufficient validation for name - check the boundaries of the full text not the boundaries of a new line like previous case

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :password, :password_confirmation, :site_url
   has_secure_password
-  validates_format_of :name, with: /^\w+$/
+  validates_format_of :name, with: /\A\w+\z/
 end
