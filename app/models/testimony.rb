b5b4a271eb0c2349d53f62daa6bd075550cb2fc1
petrero <petrero@gmail.com>
2012-09-22 17:36:14 +0300

    reducing the content returned by Twitter by not getting the tweets already returned with previous pull_tweets calls

--- app/models/testimony.rb
+++ app/models/testimony.rb
@@ -2,7 +2,7 @@ class Testimony < ActiveRecord::Base
   attr_accessible :content, :screen_name, :tweet_id
 
 	def self.pull_tweets
-		Twitter.favorites("railscasts").each do |tweet|
+		Twitter.favorites("railscasts", since_id: maximum(:tweet_id)).each do |tweet|
 			unless exists?(tweet_id: tweet.id)
 				create!(
 					tweet_id: tweet.id,
