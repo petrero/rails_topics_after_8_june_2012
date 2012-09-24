840cd8bf30097bb68e5a0423c89bc6b69caf8865
petrero <petrero@gmail.com>
2012-09-24 11:50:50 +0300

    initial store site

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,47 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+		category = Category.find_or_create_by_name!(params[:product][:category])
+		params[:product][:category_id] = category.id
+    params[:product].delete("category")
+    @product = Product.new(params[:product])
+    if @product.save
+      redirect_to @product, notice: "Product was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+		category = Category.find_or_create_by_name!(params[:product][:category])
+		params[:product][:category_id] = category.id
+    params[:product].delete(:category)
+    if @product.update_attributes(params[:product])
+      redirect_to @product, notice: "Product was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_url, notice: "Product was destroyed."
+  end
+end
