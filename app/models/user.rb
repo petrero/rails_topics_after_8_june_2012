99511002759e90f78581df6beaaa6f8a781afac0
petrero <petrero@gmail.com>
2012-09-28 11:33:34 +0300

    dealing with exceptions

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,12 @@ class User < ActiveRecord::Base
 
 	def facebook
 		@facebook ||= Koala::Facebook::API.new(oauth_token)
+		block_given? ? yield(@facebook) : @facebook
+	rescue Koala::Facebook::APIError => e
+		logger.info e.to_s 
+	end
+
+	def friends_count
+		facebook { |fb| fb.get_connection("me", "friends").size }
 	end
 end
