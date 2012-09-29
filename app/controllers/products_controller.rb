acd395731ce747d224409ec8d9b75853c2a516b0
petrero <petrero@gmail.com>
2012-09-29 14:47:20 +0300

    making a template for xls format, this way new lines are permitted in columns

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,8 +3,8 @@ class ProductsController < ApplicationController
 		@products = Product.order(:name)
 		respond_to do |format|
 			format.html
-			format.csv { send_data @products.to_csv }
-			format.xls { send_data @products.to_csv(col_sep: "\t")}
+			format.csv { send_data @products.to_csv(col_sep: ",") }
+			format.xls # { send_data @products.to_csv(col_sep: "\t")}
 		end
   end
 end
