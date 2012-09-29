a4fa61315ba4845609f661338dbe55099e9e75b2
petrero <petrero@gmail.com>
2012-09-29 22:12:45 +0300

    writing callback omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,12 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username
   # attr_accessible :title, :body
 	validates_presence_of :username
+
+	def self.from_omniauth(auth)
+		where(auth.slice(:provider, :uid)).first_or_create do |user|
+			user.provider = auth.provider
+			user.uid = auth.uid
+			user.username = auth.info.nickname
+		end
+	end
 end
