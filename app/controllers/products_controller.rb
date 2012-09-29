6bc5090151fd55158e43431accd3593a6b345979
petrero <petrero@gmail.com>
2012-09-29 14:31:58 +0300

    downloading a tab separated csv file that imitate an xls file type - this aproach has problems

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,7 @@ class ProductsController < ApplicationController
 		respond_to do |format|
 			format.html
 			format.csv { send_data @products.to_csv }
+			format.xls { send_data @products.to_csv(col_sep: "\t")}
 		end
   end
 end
