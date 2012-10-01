bc4738df052b1483a9a24085fc8c3f570116e667
petrero <petrero@gmail.com>
2012-10-01 21:16:45 +0300

    initial low performer site

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+p = Project.create! name: "Learn Rails"
+p.tasks.create! name: "Learn Ruby"
+p.tasks.create! name: "Learn HTML"
+p.tasks.create! name: "Learn CSS (and Sass)"
+p.tasks.create! name: "Learn JavaScript (and CoffeeScript)"
+p.tasks.create! name: "Learn SQL and relational databases"
+p.tasks.create! name: "Learn HTTP and request/response process"
+
+p = Project.create! name: "Learn Karate"
+p.tasks.create! name: "Find Mr. Miyagi"
+p.tasks.create! name: "Paint the fence"
+p.tasks.create! name: "Wax the car"
+p.tasks.create! name: "Sand the deck"
+
+p = Project.create! name: "Count down from 100"
+100.downto(1) do |num|
+  p.tasks.create! name: "Say #{num}"
+end
+
+p = Project.create! name: "Count down from 1,000"
+1000.downto(1) do |num|
+  p.tasks.create! name: "Say #{num}"
+end
+
