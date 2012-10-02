b9f88535af65ef7079d29390d95fa8786f57c365
petrero <petrero@gmail.com>
2012-10-02 20:21:50 +0300

    you may reach a limit on how much can you pass in a get request. To get around this one solution is to use a post request instead

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RailsTopicsAfter8June2012::Application.routes.draw do
-  resources :products
+  resources :products do
+		collection { post :search, to: 'products#index' }
+	end
   root to: 'products#index'
 end
