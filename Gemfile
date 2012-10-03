69bdf7e4b6a1cf7c67f46b14d4f1a6b0f39bd37a
petrero <petrero@gmail.com>
2012-10-03 17:31:45 +0300

    initial site

--- Gemfile
+++ Gemfile
@@ -22,8 +22,11 @@ end
 
 gem 'jquery-rails'
 
+gem 'quiet_assets', group: :development
+gem 'thin'
+
 # To use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.0.0'
+gem 'bcrypt-ruby', '~> 3.0.0'
 
 # To use Jbuilder templates for JSON
 # gem 'jbuilder'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+t = Topic.create! name: "Looking Forward to Rails 4.0", sticky: false
+t.posts.create! content: "Rails 4.0 will be great!"
+t.posts.create! content: "Yes, I can't wait"
+
+t = Topic.create! name: "Anyone tried strong_parameters?", sticky: false
+t.posts.create! content: "This plugin looks useful"
+t.posts.create! content: "It will be in Rails 4.0 likely"
+t.posts.create! content: "That's good to know!"
+
+t = Topic.create! name: "Security and Mass Assignment", sticky: false
+t.posts.create! content: "Make sure to add attr_accessible"
+
+t = Topic.create! name: "Frequently Asked Questions", sticky: true
+t.posts.create! content: "This is a sticky topic"
+
+User.create! email: "petrero@gmail.com", password: "secret"
