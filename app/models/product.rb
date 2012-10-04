bb1185c52c409b2a1031834e764d7dda2309f1a2
petrero <petrero@gmail.com>
2012-10-04 18:35:14 +0300

    viewing what attributes changed and what they were

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,9 @@ class Product < ActiveRecord::Base
 		update_attributes(*args)
 	rescue ActiveRecord::StaleObjectError
 		errors.add :base, "This record changed while you were editing."
+		changes.each do |name, values|
+			errors.add name, "was #{values.first}"
+		end
 		false
 	end
   attr_accessible :name, :price, :released_on, :category_id, :lock_version
