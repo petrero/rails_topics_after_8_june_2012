a8a6b631d8d10b8a4e1fb9bb5c503de23533b860
petrero <petrero@gmail.com>
2012-10-04 17:44:30 +0300

    attempts to edit records that doesn't have the same lock_version raise an ActiveRecord::StaleObjectError

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121004140655) do
+ActiveRecord::Schema.define(:version => 20121004143551) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -21,12 +21,13 @@ ActiveRecord::Schema.define(:version => 20121004140655) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
-    t.decimal  "price",       :precision => 2, :scale => 8
+    t.decimal  "price",        :precision => 2, :scale => 8
     t.date     "released_on"
-    t.integer  "view_count",                                :default => 0, :null => false
+    t.integer  "view_count",                                 :default => 0, :null => false
     t.integer  "category_id"
-    t.datetime "created_at",                                               :null => false
-    t.datetime "updated_at",                                               :null => false
+    t.datetime "created_at",                                                :null => false
+    t.datetime "updated_at",                                                :null => false
+    t.integer  "lock_version",                               :default => 0, :null => false
   end
 
   add_index "products", ["category_id"], :name => "index_products_on_category_id"
