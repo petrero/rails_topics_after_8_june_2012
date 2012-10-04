542a2aa4491338fa329cdec1963419b754d68c56
petrero <petrero@gmail.com>
2012-10-04 18:30:27 +0300

    rescue from ActiveRecord::StaleObjectError through a custom validation on model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ActiveRecord::Base
   belongs_to :category
+
+	def update_with_conflict_validation(*args)
+		update_attributes(*args)
+	rescue ActiveRecord::StaleObjectError
+		errors.add :base, "This record changed while you were editing."
+		false
+	end
   attr_accessible :name, :price, :released_on, :category_id, :lock_version
 end
