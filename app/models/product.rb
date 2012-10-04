2db0c5c37ab969288c22030572526ac510082d69
petrero <petrero@gmail.com>
2012-10-04 18:55:19 +0300

    rollback the changes made for optimistic locking

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,5 @@
 class Product < ActiveRecord::Base
   belongs_to :category
 
-	def update_with_conflict_validation(*args)
-		update_attributes(*args)
-	rescue ActiveRecord::StaleObjectError
-		self.lock_version = lock_version_was
-		errors.add :base, "This record changed while you were editing."
-		changes.except("updated_at").each do |name, values|
-			errors.add name, "was #{values.first}"
-		end
-		false
-	end
-  attr_accessible :name, :price, :released_on, :category_id, :lock_version
+	attr_accessible :name, :price, :released_on, :category_id
 end
