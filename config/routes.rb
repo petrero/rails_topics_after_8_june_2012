f88648f369f60a283f53a8869b85d8bd3a9bb325
petrero <petrero@gmail.com>
2012-10-05 17:18:59 +0300

    mapping about link to about action info controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 RailsTopicsAfter8June2012::Application.routes.draw do
+  get "about", to: "info#about"
+  get "info/privacy"
+  get "info/license"
+
+	
   resources :products
   root to: 'products#index'
 end
