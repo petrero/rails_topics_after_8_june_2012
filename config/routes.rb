6142343415ab5a00ffa9952987ff55c09f097cec
petrero <petrero@gmail.com>
2012-10-05 18:02:55 +0300

    moving the static content into the database and interating with it through a restful stle controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 RailsTopicsAfter8June2012::Application.routes.draw do
   
-	%w[about privacy license].each do |page|
-		get page, controller: "info", action: page
-	end
-
+  resources :pages
 
   resources :products
   root to: 'products#index'
