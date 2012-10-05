679dadd1ddb795adc7a4ed2584a40fa3e2ef8ecf
petrero <petrero@gmail.com>
2012-10-05 17:24:45 +0300

    the usual approach when dealing with static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 RailsTopicsAfter8June2012::Application.routes.draw do
-  get "about", to: "info#about"
-  get "info/privacy"
-  get "info/license"
+  
+	%w[about privacy license].each do |page|
+		get page, controller: "info", action: page
+	end
 
-	
+
   resources :products
   root to: 'products#index'
 end
