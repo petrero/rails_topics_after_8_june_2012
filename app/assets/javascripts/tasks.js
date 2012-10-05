a2380d15d9faf12bb5504fb848017609d8bcc980
petrero <petrero@gmail.com>
2012-10-05 21:29:34 +0300

    now the update will be also triggered when i click on the checkbox of the newly created tasks

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,8 +1,21 @@
-
-$(function(){
-	$('.edit_task input[type=submit]').remove();
-	$('.edit_task input[type=checkbox]').click(function(){
+jQuery.fn.submitOnCheck = function() {
+	this.find('input[type=submit]').remove();
+	this.find('input[type=checkbox]').click(function(){
 		$(this).parent('form').submit();	
 	});
+	return this
+}
+$(function(){
+	$('.edit_task').submitOnCheck();	
 })
 
+// CoffeeScript Version:
+
+//jQuery.fn.submitOnCheck = ->
+//	@find('input[type=submit]').remove()
+//	@find('input[type=checkbox]').click ->
+//		$(this).parent('form').submit()
+//	this
+
+//jQuery ->
+//	$('.edit_task').submitOnCheck()
