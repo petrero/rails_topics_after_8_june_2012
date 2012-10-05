aba5ce0a20bb4257858066142cc0678b404b879d
petrero <petrero@gmail.com>
2012-10-05 18:08:59 +0300

    modifying route to point to show action page through /about route instead of page/about url

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 RailsTopicsAfter8June2012::Application.routes.draw do
   
-  resources :pages
-
+  resources :pages, except: :show
   resources :products
   root to: 'products#index'
+
+	get ':id', to: "pages#show", as: :page
 end
