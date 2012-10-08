80f7384036188853503235c540f42ed7ace1565f
petrero <petrero@gmail.com>
2012-10-08 19:19:29 +0300

    initial site

--- Gemfile
+++ Gemfile
@@ -36,3 +36,6 @@ gem 'jquery-rails'
 
 # To use debugger
 # gem 'debugger'
+gem 'will_paginate'
+gem 'quiet_assets', group: :development
+gem 'thin'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Order.delete_all
+generator = proc do |start_time, end_time, shipping|
+  time = Time.at(rand(end_time.to_i - start_time.to_i) + start_time.to_i)
+  dollars = rand(200) + 10
+  Order.create!(price: dollars, purchased_at: time, shipping: rand(shipping))
+end
+200.times { generator.call(10.days.ago, Time.zone.now.end_of_day, 1) }
+500.times { generator.call(1.month.ago, Time.zone.now.end_of_day, 2) }
+Order.create!(price: 10, purchased_at: 1.month.ago.beginning_of_day, shipping: true)
