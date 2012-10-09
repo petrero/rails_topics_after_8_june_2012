68878ceea3b9722a46a18eff4b43c1cb52d53aca
petrero <petrero@gmail.com>
2012-10-09 19:08:48 +0300

    flush the cached category name when the name is modified

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,9 @@
 class Category < ActiveRecord::Base
   attr_accessible :name
+	has_many :products
+	after_update :flush_name_cache
+
+	def flush_after_update
+		Rails.cache.delete([:category, id, :name]) if name_changed?
+	end	
 end
