a20a36ef451bacfe4d5f4190e04edacd1d19a640
petrero <petrero@gmail.com>
2012-10-14 14:09:32 +0300

    generalize memoization approach so that we can add to any method

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,19 @@
 class Product < ActiveRecord::Base
-  def filesize(*args)
-		@filesize ||= Hash.new do |hash, key|
-			hash[key] = calculate_filesize(*key)
+  def self.memoize(name)
+		alias_method "_unmemoized_#{name}", name
+		define_method(name) do |*args|
+			@_memoized ||= {}
+			@_memoized[name] ||= Hash.new do |hash, key|
+				hash[key] = send("_unmemoized_#{name}", *key)
+			end
+		  @_memoized[name][args] 
 		end
-    @filesize[args] 
   end
 
-private
-	def calculate_filesize(num = 1)
+	def filesize(num = 1)
 		sleep 0.5
 	  4815162342 * num
-		false
 	end
+
+	memoize :filesize
 end
