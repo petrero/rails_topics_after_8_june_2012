0829a64d8e9d23c1b781ee228dc63daffe7f8d5d
petrero <petrero@gmail.com>
2012-10-14 12:20:26 +0300

    initial time consuming method filesize call

---
+++ app/helpers/products_helper.rb
@@ -0,0 +1,8 @@
+module ProductsHelper
+	def report_time(code)
+    start = Time.now
+    result = eval(code)
+    finish = Time.now
+    render "report", code: code, result: result, time: finish - start
+  end
+end
