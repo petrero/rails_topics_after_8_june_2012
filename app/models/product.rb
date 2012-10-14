a1ed1847da509dc9b92a9d9e81d8367e9319c0e4
petrero <petrero@gmail.com>
2012-10-14 13:05:26 +0300

    when method is parameter dependent the output that is read from cache will be the same no matter the value of the parameter if we don't cache parameter separately

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,11 @@
 class Product < ActiveRecord::Base
-  def filesize
-    @filesize = calculate_filesize unless defined? @filesize
-		@filesize 
+  def filesize(*args)
+    @filesize ||= calculate_filesize(*args) 
   end
 
 private
-	def calculate_filesize
+	def calculate_filesize(num = 1)
 		sleep 0.5
-	  4815162342
-		false
+	  4815162342 * num
 	end
 end
