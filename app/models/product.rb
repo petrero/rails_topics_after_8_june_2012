f8b1a63041a2be9559f07313d1d9da47ec32d07a
petrero <petrero@gmail.com>
2012-10-14 13:57:12 +0300

    using hash default value for memoization; block from the hash will be executed when key values are not found; if the method does return a false in the value that will be cached and read from

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,15 @@
 class Product < ActiveRecord::Base
   def filesize(*args)
-		@filesize ||= {}
-    @filesize[args] ||= calculate_filesize(*args) 
+		@filesize ||= Hash.new do |hash, key|
+			hash[key] = calculate_filesize(*key)
+		end
+    @filesize[args] 
   end
 
 private
 	def calculate_filesize(num = 1)
 		sleep 0.5
 	  4815162342 * num
+		false
 	end
 end
