ee6a9caa62c8b3aa6c045f4d5bf1a25855f78b86
petrero <petrero@gmail.com>
2012-10-24 15:43:56 +0300

    our intention is that a normal user can edit only topics created by himself but the controller isn't pass it the resource into can check so when we try will get not authorized

--- spec/models/permission_spec.rb
+++ spec/models/permission_spec.rb
@@ -34,13 +34,22 @@ describe Permission do
 	end	
 
 	describe "as member" do
-		subject {Permission.new(build(:user, admin: false))}
+		let(:user) { create(:user, admin: false) }
+		let(:user_topic) { build(:topic, user: user) }
+		let(:other_topic) { build(:topic) }
+		subject {Permission.new(user)}
+
+
 		it { should allow("topics", "index") }
 		it { should allow("topics", "show") }
 		it { should allow("topics", "new") }
 		it { should allow("topics", "create") }
-		it { should allow("topics", "edit") }
-		it { should allow("topics", "update") }
+		it { should_not allow("topics", "edit") }
+		it { should_not allow("topics", "update") }
+		it { should_not allow("topics", "edit", other_topic) }
+		it { should_not allow("topics", "update", other_topic) }
+		it { should allow("topics", "edit", user_topic) }
+		it { should allow("topics", "update", user_topic) }
 		it { should_not allow("topics", "destroy") }
 
 		it { should allow("sessions", "new") }
