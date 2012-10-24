856f5d9f1b458593b30f1d724a219452cb0d9cc6
petrero <petrero@gmail.com>
2012-10-24 12:10:52 +0300

    make a test in case of logged in user and make it to pass

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -2,8 +2,10 @@ class Permission < Struct.new(:user)
 	def allow?(controller, action)
 		if user.nil?
 			controller == "topics" && action.in?(%w[index show])
-		else
+		elsif user.admin?
 			true
+		else
+			controller == "topics" && action != "destroy"
 		end
 	end
 end
