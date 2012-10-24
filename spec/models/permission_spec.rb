3cdac362a8def7c319965ca7138e9229d8674e57
petrero <petrero@gmail.com>
2012-10-24 12:42:15 +0300

    add authorization for sessions and users controller

--- spec/models/permission_spec.rb
+++ spec/models/permission_spec.rb
@@ -17,17 +17,20 @@ describe Permission, focus: true do
 		it { should_not allow("topics", "edit") }
 		it { should_not allow("topics", "update") }
 		it { should_not allow("topics", "destroy") }
+
+		it { should allow("sessions", "new") }
+		it { should allow("sessions", "create") }
+		it { should allow("sessions", "destroy") }
+
+		it { should allow("users", "new") }
+		it { should allow("users", "create") }
+		it { should_not allow("users", "edit") }
+		it { should_not allow("users", "update") }
 	end	
 
 	describe "as admin" do
 		subject {Permission.new(build(:user, admin: true))}
-		it { should allow("topics", "index") }
-		it { should allow("topics", "show") }
-		it { should allow("topics", "new") }
-		it { should allow("topics", "create") }
-		it { should allow("topics", "edit") }
-		it { should allow("topics", "update") }
-		it { should allow("topics", "destroy") }
+		it { should allow("anything", "here") }
 	end	
 
 	describe "as member" do
@@ -39,5 +42,14 @@ describe Permission, focus: true do
 		it { should allow("topics", "edit") }
 		it { should allow("topics", "update") }
 		it { should_not allow("topics", "destroy") }
+
+		it { should allow("sessions", "new") }
+		it { should allow("sessions", "create") }
+		it { should allow("sessions", "destroy") }
+
+		it { should allow("users", "new") }
+		it { should allow("users", "create") }
+		it { should allow("users", "edit") }
+		it { should allow("users", "update") }
 	end		
 end
