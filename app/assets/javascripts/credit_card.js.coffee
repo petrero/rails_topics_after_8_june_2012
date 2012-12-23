4efad905b6e6335891742fcdb1716dc159b5ad4d
petrero <petrero@gmail.com>
2012-12-23 18:07:43 +0200

    checkout site with card number validation

--- app/assets/javascripts/credit_card.js.coffee
+++ app/assets/javascripts/credit_card.js.coffee
@@ -6,6 +6,7 @@ class @CreditCard
 		total = 0
 		for i in [(@number.length - 1)..0]
 			n = +@number[i]
+			console.log(n)
 			if (i + @number.length) % 2 == 0
 				n = if n * 2 > 9 then n * 2 - 9 else n * 2
 			total += n
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+24.times do |n|
+  Order.create! number: n+1, card_last_four: 1234, amount: 19.99
+end
