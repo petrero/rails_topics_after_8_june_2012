60647964b85addee74eb8b045283d3fd94134915
petrero <petrero@gmail.com>
2012-12-24 16:31:27 +0200

    testing if the next ten pages are retrieved and displayed when scrolling to the top

--- spec/requests/orders_spec.rb
+++ spec/requests/orders_spec.rb
@@ -6,4 +6,13 @@ describe "Orders" do
 		fill_in "Credit Card Number", with: "1234"
 		page.should have_content("Invalid card number")
 	end
+
+	it "fetches more orders when scrolling to the bottom", js: true do
+		11.times {|n| Order.create! number: n + 1 }
+		visit orders_path
+		page.should have_content('Order #1')
+		page.should_not have_content('Order #11')
+		page.evaluate_script("window.scrollTo(0, document.height)")
+		page.should have_content("Order #11")
+	end
 end
