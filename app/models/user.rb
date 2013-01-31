3e8bce905e8598ba35bbf744e5e28ae8e8cb8609
petrero <petrero@gmail.com>
2013-01-31 20:41:32 +0200

    creating a new account for guest users without being impeded by validations required by has_secure_password method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,18 @@ class User < ActiveRecord::Base
 
   attr_accessible :username, :email, :password, :password_confirmation
 
-  validates_presence_of :username, :email
-  validates_uniqueness_of :username
+  validates_presence_of :username, :email, unless: :guest?
+  validates_uniqueness_of :username, allow_blank: true
 
-  has_secure_password
+
+  validates_confirmation_of :password
+  validates_presence_of     :password_digest, unless: :guest?
+
+	require 'bcrypt'
+
+  attr_reader :password
+  include InstanceMethodsOnActivation
+
 
 	def self.new_guest
 		new {|u| u.guest = true}
