bab5cdf6012ab01d84f71f6f876c6c4cbb36c0e0
petrero <petrero@gmail.com>
2013-03-17 12:20:41 +0200

    refine SearchSuggestion model and controller

--- app/models/search_suggestion.rb
+++ app/models/search_suggestion.rb
@@ -1,3 +1,8 @@
 class SearchSuggestion < ActiveRecord::Base
   attr_accessible :popularity, :term
+
+	def self.terms_for(prefix)
+		suggestions = where("term like ?", "#{prefix}_%")
+		suggestions.order("popularity desc").limit(10).pluck(:term)
+	end
 end
