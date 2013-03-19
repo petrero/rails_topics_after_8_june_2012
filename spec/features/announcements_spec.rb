6b56b768e5770a85f50ebde3df91041b80638b16
petrero <petrero@gmail.com>
2013-03-19 22:19:05 +0200

    write a test for javascript version; because this functionality is already implemented but doesn't rely on javascript will pass too

--- spec/features/announcements_spec.rb
+++ spec/features/announcements_spec.rb
@@ -11,4 +11,11 @@ describe "Announcements" do
 		page.should_not have_content("Hello World")
   end
 
+	it "displays active announcements", js: true do
+    Announcement.create! message: "Hello World!", starts_at: 1.hour.ago, ends_at: 1.hour.from_now
+		visit root_path
+		page.should have_content("Hello World")
+		click_on "hide announcement"
+		page.should_not have_content("Hello World")
+  end
 end
