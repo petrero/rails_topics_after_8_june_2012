7b6f453bc38b0fb01479e6eda7e52def8ea6806e
petrero <petrero@gmail.com>
2013-03-19 22:15:09 +0200

    all is functional now but every time we hit hide annoucement we have a full refresh because of redirect, we want ti be hidden through javascript

--- spec/models/announcement_spec.rb
+++ spec/models/announcement_spec.rb
@@ -7,4 +7,16 @@ describe Announcement do
 		upcoming = Announcement.create! starts_at: 1.hour.from_now, ends_at: 1.day.from_now
 		Announcement.current.should eq([current])
 	end
+
+
+	it "does not include ids passed in to current" do
+		current_1 = Announcement.create! starts_at: 1.hour.ago, ends_at: 1.day.from_now
+		current_2 = Announcement.create! starts_at: 1.hour.ago, ends_at: 1.day.from_now
+		Announcement.current([current_2.id]).should eq([current_1])
+	end
+
+	it "includes current when nil is passed in" do
+		current = Announcement.create! starts_at: 1.hour.ago, ends_at: 1.day.from_now
+		Announcement.current(nil).should eq([current])
+	end
 end
