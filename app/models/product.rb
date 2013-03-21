0e1549dace6c0ba6cae78f6ca0e42f92eb0a0d0b
petrero <petrero@gmail.com>
2013-03-21 21:16:18 +0200

    edit the price attribute relatively for example -20%

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,16 @@
 class Product < ActiveRecord::Base
   belongs_to :category
 	validates_numericality_of :price
-  attr_accessible :discontinued, :name, :price, :category_id
+  attr_accessible :discontinued, :name, :price, :category_id, :price_modification
+
+	attr_reader :price_modification
+
+	def price_modification=(new_price)
+		@price_modification = new_price
+		if new_price.to_s.ends_with? "%"
+			self.price += (price * (new_price.to_d/100)).round(2)
+		else
+			self.price = new_price
+		end
+	end
 end
