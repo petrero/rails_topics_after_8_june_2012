3771a143699e37de771b54c97dd29af6160586ca
petrero <petrero@gmail.com>
2013-03-22 22:43:55 +0200

    get only create and destroy tracked on the comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
 	include PublicActivity::Model
-	tracked owner: ->(controller, model) { controller && controller.current_user }
+	tracked except: :update, owner: ->(controller, model) { controller && controller.current_user }
 
   belongs_to :user
   belongs_to :recipe
