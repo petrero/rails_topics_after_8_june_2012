77f74f7dc9deda72692393b87077422e49782f3b
petrero <petrero@gmail.com>
2013-03-23 00:00:53 +0200

    set up polymorphic association on models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
-  
+
   attr_accessible :name, :password, :password_confirmation
-  
+
   validates_uniqueness_of :name
 
+	has_many :activities
   has_many :recipes
   has_many :comments
   has_many :friendships
