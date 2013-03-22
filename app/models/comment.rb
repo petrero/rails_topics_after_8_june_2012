de437386fd8cd5e0e7e14c7d453f83e7f818ca1a
petrero <petrero@gmail.com>
2013-03-22 22:59:57 +0200

    perform tracking activities through controller instead of callbacks on the model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
-	include PublicActivity::Model
-	tracked except: :update, owner: ->(controller, model) { controller && controller.current_user }
+	include PublicActivity::Common
+	#tracked except: :update, owner: ->(controller, model) { controller && controller.current_user }
 
   belongs_to :user
   belongs_to :recipe
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ActiveRecord::Base
-	include PublicActivity::Model
-	tracked owner: ->(controller, model) { controller && controller.current_user }
+	include PublicActivity::Common
+	#tracked owner: ->(controller, model) { controller && controller.current_user }
 
   attr_accessible :description, :image_url, :name
 
