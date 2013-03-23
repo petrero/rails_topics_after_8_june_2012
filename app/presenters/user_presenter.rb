b38eded246382f114087ed79af5ace58c0ff2997
petrero <petrero@gmail.com>
2013-03-23 10:45:25 +0200

    reference generic @object instances in preseneters not only @user

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -1,13 +1,15 @@
 class UserPresenter < BasePresenter
+	presents :user
+
 	def avatar
-		h.link_to_if @user.url.present?, h.image_tag("avatars/#{avatar_name}", class: "avatar"), @user.url
+		h.link_to_if user.url.present?, h.image_tag("avatars/#{avatar_name}", class: "avatar"), user.url
 	end
 
 private
 
 	def avatar_name
-    if @user.avatar_image_name.present?
-      @user.avatar_image_name
+    if user.avatar_image_name.present?
+      user.avatar_image_name
     else
       "default.png"
     end
