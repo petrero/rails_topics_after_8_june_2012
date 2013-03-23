9c7d393da2918fab7dd12d33cfe80cad3820b1a9
petrero <petrero@gmail.com>
2013-03-23 10:19:58 +0200

    move complex logic view related to displaying avatar image into presenter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,7 +16,7 @@ module ApplicationHelper
 	def present(object, klass = nil)
 		klass ||= "#{object.class}Presenter".constantize
 		presenter = klass.new(object, self)
-		yield presenter if block.given?
+		yield presenter if block_given?
 		presenter
 	end
 end
--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -3,4 +3,22 @@ class UserPresenter
 		@user = user
 		@template = template
 	end
+
+	def h
+		@template
+	end
+
+	def avatar
+		h.link_to_if @user.url.present?, h.image_tag("avatars/#{avatar_name}", class: "avatar"), @user.url
+	end
+
+private
+
+	def avatar_name
+    if @user.avatar_image_name.present?
+      @user.avatar_image_name
+    else
+      "default.png"
+    end
+  end
 end
