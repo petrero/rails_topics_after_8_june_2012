46d3555950decb2e1c1a94f5b154a38d246e4388
petrero <petrero@gmail.com>
2013-03-23 12:24:46 +0200

    the view object passed into the presenter will have access to all the helper methods in the application except those defined in the controller so current_user must be stubbed out and supply any desired values even when you call helper_method in the controller

--- spec/presenters/user_presenter_spec.rb
+++ spec/presenters/user_presenter_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe UserPresenter do
 
 	it "says when none given" do
+		#view.current_user #will not work
 		presenter = UserPresenter.new(User.new, view)
 		presenter.website.should include("None given")
 	end
