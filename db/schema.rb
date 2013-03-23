0cb772382eb9e85fe9b1a82a171c9330652fe61a
petrero <petrero@gmail.com>
2013-03-23 17:48:10 +0200

    I want a product type to have many different product fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130323152601) do
+ActiveRecord::Schema.define(:version => 20130323154124) do
+
+  create_table "product_types", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "products", :force => true do |t|
     t.string   "name"
