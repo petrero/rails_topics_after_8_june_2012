5402c105f629e699f0ce34b40587e9d9524f7882
petrero <petrero@gmail.com>
2013-03-24 12:47:08 +0200

    rails g resource entry name winner:boolean

---
+++ app/assets/javascripts/models/entry.js
@@ -0,0 +1,4 @@
+RailsTopicsAfter8June2012.Entry = DS.Model.extend({
+  name: DS.attr('string'),
+  winner: DS.attr('boolean')
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 RailsTopicsAfter8June2012::Application.routes.draw do
+  resources :entries
+
+
 	root to: "application#index"
 
   # The priority is based upon order of creation:
